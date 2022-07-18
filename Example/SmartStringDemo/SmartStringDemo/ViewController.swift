ef212b12347a6fc844976200c92a3a62f688d21d
Valerio <valerio@Valerios-MBP-2.home>
2022-07-18 10:55:58 +0200

    Update Demo project

--- Example/SmartStringDemo/SmartStringDemo/ViewController.swift
+++ Example/SmartStringDemo/SmartStringDemo/ViewController.swift
@@ -1,25 +1,8 @@
 //
 //  ViewController.swift
+//  SmartStringDemo
 //
-//  Copyright (c) 2021 Valerio69 (valerio.alsebas@gmail.com)
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy
-//  of this software and associated documentation files (the "Software"), to deal
-//  in the Software without restriction, including without limitation the rights
-//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//  copies of the Software, and to permit persons to whom the Software is
-//  furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in
-//  all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//  THE SOFTWARE.
+//  Created by Valerio Sebastianelli on 2/3/22.
 //
 
 import UIKit
@@ -38,15 +21,10 @@ class ViewController: UIViewController {
 //            })
 //        }
         
-        let style1 = SmartStringStyle { style in
-            style.color = .green
-            style.font = .boldSystemFont(ofSize: 30)
-            style.shadow = .default
-        }
-                
-        label.smartString = "Hello" + " world!".style(style1)
-
 //        label.smartString = "<primary>Hello </primary><secondary>world!</secondary>".smartStringXML
+        
+        let xmlString = "Hello ".tag(with: .primary) + "world!".tag(with: .secondary)
+        label.smartString = xmlString.smartStringXML
 
         label.sizeToFit()
         label.numberOfLines = 0
