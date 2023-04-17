ce6d5d14649669d003fcf3b99caa2c2ee24f34c1
Valerio <valerio@Valerios-MacBook-Pro-2.local>
2023-04-17 10:01:24 +0200

    improve protocol documentation

--- Sources/Protocols/SmartStringable.swift
+++ Sources/Protocols/SmartStringable.swift
@@ -29,109 +29,121 @@ public protocol SmartStringable {
   
   // MARK: - Style
   
-  /// Set SmartString style
-  /// - Parameter style: The a SmartStringStyle object.
-  /// - Returns: The new SmartString instance.
+  /// Sets a SmartString style to the text.
+  /// - Parameter style: A SmartStringStyle object.
+  /// - Returns: A new SmartString instance with the applied style.
   func style(_ style: SmartStringStyle) -> SmartString
   
+  /// Sets text alignment.
+  /// - Parameter alignment: A NSTextAlignment value.
+  /// - Returns: A new SmartString instance with the applied text alignment.
   func align(_ alignment: NSTextAlignment) -> SmartString
   
+  /// Sets line spacing between text lines.
+  /// - Parameter lineSpacing: A CGFloat value representing line spacing.
+  /// - Returns: A new SmartString instance with the applied line spacing.
   func lineSpacing(_ lineSpacing: CGFloat) -> SmartString
   
+  /// Sets a fixed line height for the text.
+  /// - Parameter height: A CGFloat value representing line height.
+  /// - Returns: A new SmartString instance with the applied line height.
   func lineHeight(_ height: CGFloat) -> SmartString
   
+  /// Sets character spacing (kerning) for the text.
+  /// - Parameter space: A CGFloat value representing character spacing.
+  /// - Returns: A new SmartString instance with the applied character spacing.
   func charactersSpacing(_ space: CGFloat) -> SmartString
   
   // MARK: - Color
   
-  /// Apply a new color.
-  /// - Parameter color: The new color.
-  /// - Returns: The new SmartString instance.
+  /// Sets the text color.
+  /// - Parameter color: A UIColor object representing the text color.
+  /// - Returns: A new SmartString instance with the applied text color.
   func color(_ color: UIColor) -> SmartString
   
-  /// Apply a new Color using a Closure.
-  /// - Parameter closure: The closure which must return the new Color.
-  /// - Returns: The new SmartString instance.
+  /// Sets the text color using a closure.
+  /// - Parameter closure: A closure that returns a UIColor object.
+  /// - Returns: A new SmartString instance with the applied text color.
   func color(closure: () -> UIColor) -> SmartString
   
-  /// Apply a new Background color.
-  /// - Parameter closure: The new Background color.
-  /// - Returns: The new SmartString instance.
+  /// Sets the background color of the text.
+  /// - Parameter color: A UIColor object representing the background color.
+  /// - Returns: A new SmartString instance with the applied background color.
   func background(_ color: UIColor) -> SmartString
   
-  /// Apply a new Background color using a Closure.
-  /// - Parameter closure: The closure which must return the new Background Color.
-  /// - Returns: The new SmartString instance.
+  /// Sets the background color of the text using a closure.
+  /// - Parameter closure: A closure that returns a UIColor object.
+  /// - Returns: A new SmartString instance with the applied background color.
   func background(closure: () -> UIColor) -> SmartString
   
   // MARK: - Font
   
-  /// Apply a new Font.
-  /// - Parameter closure: The new Font.
-  /// - Returns: The new SmartString instance.
+  /// Sets the font of the text.
+  /// - Parameter font: A UIFont object representing the font.
+  /// - Returns: A new SmartString instance with the applied font.
   func font(_ font: UIFont) -> SmartString
   
-  /// Apply a new Font using a Closure.
-  /// - Parameter closure: The closure which must return the new Font.
-  /// - Returns: The new SmartString instance.
+  /// Sets the font of the text using a closure.
+  /// - Parameter closure: A closure that returns a UIFont object.
+  /// - Returns: A new SmartString instance with the applied font.
   func font(closure: () -> UIFont) -> SmartString
   
-  /// Apply a bold effect to the Font. **Works only if a Font was previously applyed**
-  /// - Returns: SmartString instance
+  /// Applies a bold effect to the font. **Only works if a font was previously applied**
+  /// - Returns: A new SmartString instance with the bold effect applied.
   func bold() -> SmartString
   
-  /// Apply the Underline effect to the string.
-  /// - Returns: The new SmartString instance.
+  /// Applies an underline effect to the text.
+  /// - Returns: A new SmartString instance with the underline effect applied.
   func underline() -> SmartString
   
-  /// Apply an italic effect to the Font. **Works only if a Font was previously applyed**
-  /// - Returns: SmartString instance
+  /// Applies an italic effect to the font. **Only works if a font was previously applied**
+  /// - Returns: A new SmartString instance with the italic effect applied.
   func italic() -> SmartString
   
   // MARK: - Shadow
   
-  /// Apply a Shadow.
-  /// - Parameter closure: The shadow style.
-  /// - Returns: The new SmartString instance.
+  /// Applies a shadow to the text.
+  /// - Parameter shadow: A SmartShadow object representing the shadow style.
+  /// - Returns: A new SmartString instance with the applied shadow.
   func shadow(_ shadow: SmartShadow) -> SmartString
   
-  /// Apply a Shadow using a Closure.
-  /// - Parameter closure: The closure which must return the new Shadow Style.
-  /// - Returns: The new SmartString instance.
+  /// Applies a shadow to the text using a closure.
+  /// - Parameter closure: A closure that returns a SmartShadow object.
+  /// - Returns: A new SmartString instance with the applied shadow.
   func shadow(closure: () -> SmartShadow) -> SmartString
   
   // MARK: - Link
   
-  /// Apply a link type effect and make the url tappable.
-  /// - Parameter closure: The URL to open using the default browser.
-  /// - Returns: The new SmartString instance.
+  /// Applies a link effect to the text and makes it tappable.
+  /// - Parameter url: The URL to be opened in the default browser.
+  /// - Returns: A new SmartString instance with the link effect applied.
   func link(_ url: URL) -> SmartString
   
-  /// Apply a link type effect and make the url tappable using a Closure.
-  /// - Parameter closure: The closure which must return the URL to open using the default browser.
-  /// - Returns: The new SmartString instance.
+  /// Applies a link effect to the text and makes it tappable using a closure.
+  /// - Parameter closure: A closure that returns the URL to be opened in the default browser.
+  /// - Returns: A new SmartString instance with the link effect applied.
   func link(closure: () -> URL) -> SmartString
   
   // MARK: - Image
   
-  /// Append an Image of a given height to the string.
+  /// Appends an image with a specified height to the text.
   /// - Parameters:
-  ///   - image: The image to append
-  ///   - height: The image height to use. Default is 18.
-  /// - Returns: The new SmartString instance.
+  /// - image: The UIImage object to be appended.
+  /// - height: The height of the image (default is 18).
+  /// - Returns: A new SmartString instance with the image appended.
   func appendImage(_ image: UIImage, height: CGFloat) -> SmartString
   
-  /// Append an Image of a given height to the string using a Closure
+  /// Appends an image with a specified height to the text using a closure.
   /// - Parameters:
-  ///   - height: The image height to use. Default is 18.
-  ///   - closure: The closure which must return the Image to append.
-  /// - Returns: The new SmartString instance.
+  /// - height: The height of the image (default is 18).
+  /// - closure: A closure that returns the UIImage object to be appended.
+  /// - Returns: A new SmartString instance with the image appended.
   func appendImage(height: CGFloat, closure: () -> UIImage) -> SmartString
   
   // MARK: - Tap Handler
   
-  /// Add Tap Geture to the SmartString object.
-  /// - Parameter closure: The closure called when user tap on the string.
-  /// - Returns: The new SmartString instance.
+  /// Adds a tap gesture to the SmartString object.
+  /// - Parameter closure: A closure that is called when the user taps on the text. The closure takes a String parameter.
+  /// - Returns: A new SmartString instance with the tap gesture added.
   func onTap(closure: @escaping (String) -> Void) -> SmartString
 }
