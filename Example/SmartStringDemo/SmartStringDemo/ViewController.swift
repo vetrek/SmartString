20fc9e24fe01305bc71cafdb9171d3c75b2a86d6
Valerio <valerio@Valerios-MacBook-Pro-2.local>
2023-04-17 09:54:35 +0200

    update example

--- Example/SmartStringDemo/SmartStringDemo/ViewController.swift
+++ Example/SmartStringDemo/SmartStringDemo/ViewController.swift
@@ -9,24 +9,26 @@ import UIKit
 import SmartString
 
 class ViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        let str: SmartString = "Hello".align(.right) + "\nWorld!".align(.center)
-
-        let label = UILabel()
-        label.smartString = str
-        label.sizeToFit()
-        label.numberOfLines = 0
-        label.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(label)
-        label.center(in: view)
-        label.backgroundColor = .green
-        
-        NSLayoutConstraint.activate([
-            label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
-        ])
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    let str: SmartString = "SmartString".align(.right).color(.black)
+    + "\nSmartString!".lineHeight(40).background(.red)
+    + "\nSmartString!".lineSpacing(40).charactersSpacing(10).background(.blue)
+    + "\nSmartString!".align(.center).color(.black)
+    
+    let label = UILabel()
+    label.smartString = str
+    label.sizeToFit()
+    label.numberOfLines = 0
+    label.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(label)
+    label.center(in: view)
+    label.backgroundColor = .green
+    
+    NSLayoutConstraint.activate([
+      label.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+      label.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+    ])
+  }
 }
-
